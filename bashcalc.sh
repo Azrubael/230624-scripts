28bb5f77d2ef0302dd9eeb428576855d885f31f1
Azrubael <116581559+Azrubael@users.noreply.github.com>
2023-06-28 20:03:21 +0300

    Update bashcalc.sh
    Minor cosmetic changes

--- bashcalc.sh
+++ bashcalc.sh
@@ -1,20 +1,18 @@
 # !/bin/bash
- 
-# Take user Input
+
 echo "Enter Two numbers : "
 read a
 read b
  
 # Input type of operation
 echo "Enter Choice :"
-echo "1. Addition"
-echo "2. Subtraction"
-echo "3. Multiplication"
-echo "4. Division"
+echo "[1] Addition"
+echo "[2] Subtraction"
+echo "[3] Multiplication"
+echo "[4] Division"
 read ch
  
-# Switch Case to perform
-# calculator operations
+# Calculator operations
 case $ch in
   1)res=`echo $a + $b | bc`
   ;;
